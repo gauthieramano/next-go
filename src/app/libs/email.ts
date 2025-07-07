bf01b72a1c4c9d4a2bea727f86aa8d83beb389a3
Gauthier Amano <gauthier.net@gmail.com>
2025-07-07 04:46:11 +0200

    feat(app/libs): ✨ use `resend` to send emails

--- src/app/libs/email.ts
+++ src/app/libs/email.ts
@@ -1,29 +1,24 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-type EmailPayload = {
+type Args = {
   to: string;
   subject: string;
   html: string;
 };
 
-// Replace with your SMTP credentials
-const smtpOptions = {
-  host: process.env.EMAIL_SERVER_HOST,
-  port: parseInt(process.env.EMAIL_SERVER_PORT || "2525"),
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_SERVER_USER,
-    pass: process.env.EMAIL_SERVER_PASSWORD,
-  },
-};
+const resend = new Resend(process.env.RESEND_API_KEY);
+const EMAIL_FROM = process.env.EMAIL_FROM!;
 
-export const sendEmail = async (data: EmailPayload) => {
-  const transporter = nodemailer.createTransport({
-    ...smtpOptions,
+export const sendEmail = async (emailPayload: Args) => {
+  const { error } = await resend.emails.send({
+    from: EMAIL_FROM,
+    ...emailPayload,
   });
 
-  return await transporter.sendMail({
-    from: process.env.EMAIL_FROM,
-    ...data,
-  });
+  if (error) {
+    const { message, name, ...rest } = error;
+    const label = ["Resend", name, ...Object.values(rest)].join("/");
+
+    throw new Error(`[${label}] ${message}`);
+  }
 };
